7d3709e9566c276fe9030e3c5e140d236856da7f
102631479 <78398324+102631479@users.noreply.github.com>
2021-11-30 17:44:35 +0800

    测试

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
-    baseURL: 'http://192.168.31.147:8888/api',
+    baseURL: 'http://172.16.16.128:8888/api',
     timeout: 5000
 });
 let getToken = () => {
